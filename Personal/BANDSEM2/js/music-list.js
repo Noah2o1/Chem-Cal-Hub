23099e99d38720921617ead8985d851b4e1529b8
Noah2o1 <140681376+Noah2o1@users.noreply.github.com>
2024-02-13 10:39:53 -0500

    pp

--- Personal/BANDSEM2/js/music-list.js
+++ Personal/BANDSEM2/js/music-list.js
@@ -17,7 +17,28 @@ let allMusic = [
     src: "04 Dark Ride by Randall D. Standridge - CHIJ Katong Convent Concert Band"
   },
   {
-    
+    name: "New Orelas Strut",
+    artist: "",
+    img: "music-1",
+    src: "New Orleans Strut - Concert Band"
+  },
+  {
+    name: "Celtic Ballad (AMS)",
+    artist: "Wesley Kennedy",
+    img: "music-1",
+    src: "Celtic Ballad (AMS)"
+  },
+  {
+    name: "Drums of Corona",
+    artist: "Michael Sweeney",
+    img: "music-1",
+    src: "Drums of Corona-Michael Sweeney (ASCAP)"
+  },
+  {
+    name: "Gymnopédie No. 1",
+    artist: "",
+    img: "music-1",
+    src: "Gymnopédie No. 1"
   },
 
   // like this paste it and remember to give comma after ending of this bracket }
