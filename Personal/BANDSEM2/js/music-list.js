4849b0c02441a0d45f71873775666ddc45881240
Noah2o1 <140681376+Noah2o1@users.noreply.github.com>
2024-02-21 21:49:50 -0500

    1

--- Personal/BANDSEM2/js/music-list.js
+++ Personal/BANDSEM2/js/music-list.js
@@ -1,47 +1,70 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
 
 let allMusic = [
+  {
+    name: "DarkRide",
+    artist: "Randall D. Standridge",
+    img: "music-1",
+    src: "04 Dark Ride by Randall D. Standridge - CHIJ Katong Convent Concert Band"
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  {
+    name: "",
+    artist: "",
+    img: "",
+    src: ""
+  },
+  
 
-  {
-    name: "Dark Ride",
-    artist: "Randall Standridge",
-    img: "music-1",
-    src: "music-1"
-  },
-  {
-    name: "Celtic Ballad",
-    artist: "Wesley Kennedy",
-    img: "music-1",
-    src: "music-2"
-  },
-  {
-    name: "Drums of Corona",
-    artist: "Michael Sweeney",
-    img: "music-1",
-    src: "music-3"
-  },
-  {
-    name: "Quintessence",
-    artist: "unknown",
-    img: "music-1",
-    src: "music-4"
-  },
-  {
-    name: "Gymnopédie No. 1",
-    artist: "unknown",
-    img: "music-1",
-    src: "music-5"
-  },
-  {
-    name: "New Orleans Strut",
-    artist: "unknown",
-    img: "music-1",
-    src: "music-6"
-  },
-  {
-    name: "The Magnificent Seven",
-    artist: "unknown",
-    img: "music-1",
-    src: "music-7"
-  },
-
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
 ];
